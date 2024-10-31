4e5201c5d366acdcdf5828154d78c74de8710c93
Hernán López <mahernan@indra.es>
2024-10-31 13:48:27 +0100

    'Migracion'

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 from myapp.config import DevConfig
 
@@ -11,14 +12,18 @@ app.config.from_object(DevConfig)
 
 #db
 db=SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
 
 #blueprints
 from myapp.tasks.controllers import taskRoute
 app.register_blueprint(taskRoute)
 
-#create db
-with app.app_context():
-    db.create_all()
+#create db (Al usar Migrate esta parte ya no tendríamos que tenerla. Aquí creaba la tabla y los elementos cada vez
+# que recargaba la página. Ahora con Migrate lo haremos manualmente)
+#with app.app_context():
+#    db.create_all()
 
 #route
 @app.route('/')
